48d3508a31fe5e7665a8844694ee9c26c797bc16
DominikFrackowiak <rorschach@op.pl>
2026-05-21 11:09:55 +0200

    home almost finished (desktop)

---
+++ src/constants/experience.ts
@@ -0,0 +1,33 @@
+export const experience = [
+ {
+  company: 'experience.company.softcollie',
+  position: 'experience.position.softcollie',
+  description: 'experience.description.softcollie',
+  technologies: ['Angular', 'TypeScript', 'Tailwind CSS', 'Storybook', 'Claude Code', 'Antigravity', 'Atomic Design']
+ },
+ {
+  company: 'experience.company.alten',
+  position: 'experience.position.alten',
+  description: 'experience.description.alten',
+  technologies: ['Astro', 'React', 'TypeScript', 'Zustand', 'Storybook', 'Zod', 'Tailwind CSS', 'Atomic Design']
+ },
+ {
+  company: 'experience.company.awakelab',
+  position: 'experience.position.awakelab',
+  description: 'experience.description.awakelab',
+  technologies: ['Next.js', 'React', 'Astro', 'TypeScript', 'Supabase', 'Tailwind CSS', 'Bubble', 'Wix', 'Atomic Design']
+ },
+ {
+  company: 'experience.company.incloudforever',
+  position: 'experience.position.incloudforever',
+  description: 'experience.description.incloudforever',
+  technologies: ['Next.js', 'React', 'TypeScript', 'GraphQL', 'SCSS', 'Stripe', 'Panda CSS', 'Atomic Design']
+ },
+ {
+  company: 'experience.company.selfemployed',
+  position: 'experience.position.selfemployed',
+  description: 'experience.description.selfemployed',
+  technologies: ['HTML', 'CSS', 'JavaScript', 'Firebase', 'Firestore']
+ }
+
+] as const
---
+++ src/constants/technologies.ts
@@ -0,0 +1,29 @@
+export const frontend = [
+ 'JavaScript',
+ 'TypeScript',
+ 'React',
+ 'Next.js',
+ 'Astro',
+ 'Tailwind',
+ 'SCSS'
+] as const
+
+export const backend = [
+ 'Firebase',
+ 'Supabase',
+ 'Prisma',
+ 'Headless CMS'
+] as const
+
+export const tools = [
+ "Storybook",
+ "Zod",
+ "Git",
+ "WordPress",
+ "Figma"
+] as const
+
+export const methodologies = [
+ "BEM",
+ "Atomic Design"
+] as const
